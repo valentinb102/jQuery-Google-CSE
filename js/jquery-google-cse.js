29715bfea71ba1a722c534358c697e4b1675c9e7
valentinb102 <vben@live.com>
2013-02-01 03:38:07 -0500

    adding selectors for the various items

--- js/jquery-google-cse.js
+++ js/jquery-google-cse.js
@@ -1,17 +1,25 @@
 ;(function($, window, document, undefined) {'use strict';
 
+	/**
+	 * Required elements
+	 *
+	 * $form - the form to check for a value, could be input or form
+	 *
+	 * $results - where the results go
+	 *
+	 * $pagination - where to put the pagination
+	 */
+
 	var defaults = {
 		params : {
 			key : "AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY",
 			cx : "017262666028140812660:hb_khvgblte",
 		},
 		numResults : 10,
-		numPages : 5,
-		$form : $(window)
+		numPages : 5
 	};
 
-	function Plugin(element, options) {
-		this.element = element;
+	function Plugin(options) {
 		this.options = $.extend({}, defaults, options);
 		this._defaults = defaults;
 		this.init();
@@ -21,17 +29,16 @@
 	Plugin.prototype.init = function() {
 		var self = this;
 
-		console.log('hello');
 		// initial bind and check for hash changes
-		self.options.$form.hashchange(hashLoad);
+		$(window).hashchange(hashLoad);
 		hashLoad();
 
 		// attach listener to form
-		$(window).submit(function(e) {
+		self.options.$form.submit(function(e) {
 			e.preventDefault();
 
-			if ($("#query").val().length) {
-				location.hash = encodeURIComponent($("#query").val()) + "#1";
+			if (self.options.$form.val().length) {
+				location.hash = encodeURIComponent(self.options.$form.val()) + "#1";
 			}
 		});
 
@@ -41,8 +48,10 @@
 				if (hashArr.length > 2) {
 					ajaxSearch(hashArr[1], hashArr[2]);
 				}
-			} else
-				$("#content, #paginate").html("");
+			} else {
+				self.options.$results.html("");
+				self.options.$pagination.html("");
+			}
 		}
 
 		function ajaxSearch(query, startIndex) {
@@ -59,32 +68,33 @@
 
 			// form request and send
 			$.getJSON("https://www.googleapis.com/customsearch/v1element?key=" + self.options.params.key + "&cx=" + self.options.params.cx + "&num=" + num + "&start=" + (startIndex * self.options.numResults) + "&q=" + query + "&callback=?", function success(response) {
-				$(".loaderImg").hide();
-				$("#content, #paginate").html("");
+				$("#loader").hide();
+				self.options.$results.html("");
+				self.options.$pagination.html("");
 
 				// Search time and results found
 				// using in is the correct method for feature detection
 				if ("cursor" in response) {
-					$("#content").append("<p>Search time: " + response.cursor.searchResultTime + " seconds.<br/>" + "Results found: " + response.cursor.resultCount + "</p>");
+					self.options.$results.append("<p>Search time: " + response.cursor.searchResultTime + " seconds.<br/>" + "Results found: " + response.cursor.resultCount + "</p>");
 				}
 
 				// Spelling suggestions
 				if ("spelling" in response) {
-					$("#content").append("<p>Did you mean <i><a href='#" + response.spelling.correctedQuery + "#1'>" + response.spelling.correctedQuery + "</a></i>?</p>");
+					self.options.$results.append("<p>Did you mean <i><a href='#" + response.spelling.correctedQuery + "#1'>" + response.spelling.correctedQuery + "</a></i>?</p>");
 				}
 
 				// The results
 				if ("results" in response) {
 					for (var i = 0; i < response.results.length; i++) {
 						var item = response.results[i];
-						$("#content").append("<div class='result'><div class='container'><a class='resultLink' href='" + item.unescapedUrl + "'>" + item.title + "</a><p class='desc'>" + item.content + "</p><p class='textLink'>" + item.formattedUrl + "</p></div></div>");
+						self.options.$results.append("<div class='result'><div class='container'><a class='resultLink' href='" + item.unescapedUrl + "'>" + item.title + "</a><p class='desc'>" + item.content + "</p><p class='textLink'>" + item.formattedUrl + "</p></div></div>");
 
 						// self property is a more common location for finding an image
 						// Binding load before setting attrib seems to correctly fire load
 						// fadeto does not remove the image unlike hide
 						if ("richSnippet" in item && "cseThumbnail" in item.richSnippet) {
 							$(".result").eq(i).prepend($("<img/>").fadeTo(0, 0).load(function() {
-								$(self).fadeTo("med", 1);
+								$(this).fadeTo("med", 1);
 							}).attr("src", item.richSnippet.cseThumbnail.src));
 						}
 					}
@@ -96,7 +106,7 @@
 					var max = response.cursor.estimatedResultCount > 100 ? 100 / self.options.numResults : response.cursor.estimatedResultCount / self.options.numResults;
 
 					for (var i = 0; i < max; i++) {
-						$("<a/>").addClass("page").html(i + 1).attr("href", "#" + query + "#" + (i + 1)).appendTo($("#paginate"));
+						$("<a/>").addClass("page").html(i + 1).attr("href", "#" + query + "#" + (i + 1)).appendTo(self.options.$pagination);
 					}
 
 					// Add active page
@@ -118,18 +128,18 @@
 				// Add prev / next / first / last links
 				if ("cursor" in response && response.cursor.estimatedResultCount > self.options.numResults * $(".page").length && self.options.numResults * $(".page").length < 100 && response.cursor.estimatedResultCount > self.options.numResults) {
 					mid++;
-					$("<a/>").addClass("navLink").html("&lt;").attr("href", "#" + query + "#" + ((mid - 1 < 1) ? 1 : mid - 1)).prependTo($("#paginate"));
-					$("<a/>").addClass("navLink").html("&gt;").attr("href", "#" + query + "#" + ((mid + 1 > max) ? max : mid + 1)).appendTo($("#paginate"));
-					$("<a/>").addClass("navLink").html("&lt;&lt;").attr("href", "#" + query + "#1").prependTo($("#paginate"));
-					$("<a/>").addClass("navLink").html("&gt;&gt;").attr("href", "#" + query + "#" + max).appendTo($("#paginate"));
+					$("<a/>").addClass("navLink").html("&lt;").attr("href", "#" + query + "#" + ((mid - 1 < 1) ? 1 : mid - 1)).prependTo(self.options.$pagination);
+					$("<a/>").addClass("navLink").html("&gt;").attr("href", "#" + query + "#" + ((mid + 1 > max) ? max : mid + 1)).appendTo(self.options.$pagination);
+					$("<a/>").addClass("navLink").html("&lt;&lt;").attr("href", "#" + query + "#1").prependTo(self.options.$pagination);
+					$("<a/>").addClass("navLink").html("&gt;&gt;").attr("href", "#" + query + "#" + max).appendTo(self.options.$pagination);
 				}
 
 				// "valid" errors
 				if ("error" in response) {
-					$("#content").html("<p>Error Code: " + response.error.code + "<br/>" + response.error.message + "</p>");
+					self.options.$results.html("<p>Error Code: " + response.error.code + "<br/>" + response.error.message + "</p>");
 				}
 			}).error(function(xhr, status, errorThrown) {
-				$("#content").html("<p>" + errorThrown + "<br/>" + status + "<br/>" + xhr.statusText + "</p>");
+				self.options.$results.html("<p>" + errorThrown + "<br/>" + status + "<br/>" + xhr.statusText + "</p>");
 			});
 		}
 
@@ -137,7 +147,7 @@
 
 	$.googleCSE = function(options) {
 		// Return the instance
-		return new Plugin($('body'), options);
+		return new Plugin(options);
 	};
 
 })(jQuery, window, document);
